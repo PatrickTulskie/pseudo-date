03d0d3520578d70a687be9d0d5c8f0d0d7d21b01
Patrick Tulskie <patricktulskie@gmail.com>
2014-06-19 12:00:59 -0400

    Use RSpec 3 and load byebug for debugging in test mode

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,10 @@
 require 'rubygems'
 require 'rspec'
 require File.dirname(__FILE__) + '/../lib/pseudo_date'
+require 'byebug'
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = :should
+  end
+end
